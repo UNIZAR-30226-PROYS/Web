c10b92fea469e435a73afc8661030b32708e1266
Fidelz <fidel_97brea@hotmail.com>
2018-04-02 15:31:27 +0200

    añadido modificar contraseña en pagina perfil

---
+++ js/ventanacontrasena.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	$('.button2').click(function() {
+
+		type = $(this).attr('data-type');
+
+		$('.overlay-container2').fadeIn(function() {
+
+			window.setTimeout(function(){
+				$('.window-container2.'+type).addClass('window-container2-visible');
+			}, 100);
+
+		});
+	});
+
+	$('.close2').click(function() {
+		$('.overlay-container2').fadeOut().end().find('.window-container2').removeClass('window-container2-visible');
+	});
+
+});
