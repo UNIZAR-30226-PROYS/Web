2b5feea71fbc1342a066f9dfbb2571683975bff1
Fidelz <fidel_97brea@hotmail.com>
2018-04-17 13:06:45 +0200

    corregidos nombres parametros form registro

--- js/response_servlet.js
+++ js/response_servlet.js
@@ -5,7 +5,17 @@ $(function() {
   });
 
 
-  $('#acceso').ajaxForm(function() {
-      alert("Thank you for your comment!");
+  //$('#acceso').ajaxForm(function() {
+  //    alert("Thank you for your comment!");
+  //});
+
+  $(document).on("submit", "#acceso", function(event) {
+      var $form = $(this);
+  
+      $.post($form.attr("action"), $form.serialize(), function(response) {
+          alert("Thank you for your comment!");
+      });
+
+      event.preventDefault(); // Important! Prevents submitting the form.
   });
 });
