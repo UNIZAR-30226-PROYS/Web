c31dfc45a669793d041ea3fff73101b8309f9547
Fidelz <fidel_97brea@hotmail.com>
2018-04-18 08:07:40 +0200

    inicio falta que permita comunicacion CORS

--- js/response_servlet.js
+++ js/response_servlet.js
@@ -4,18 +4,28 @@ $(function() {
       alert("Thank you for your comment!");
   });
 
+  /*$('#form_acceso').ajaxForm(function() {
+      alert("Hecho");
+  });*/
 
   //$('#acceso').ajaxForm(function() {
   //    alert("Thank you for your comment!");
   //});
-
+/*
   $(document).on("submit", "#acceso", function(event) {
       var $form = $(this);
-  
+
       $.post($form.attr("action"), $form.serialize(), function(response) {
           alert("Thank you for your comment!");
       });
 
       event.preventDefault(); // Important! Prevents submitting the form.
-  });
+  });*/
 });
+
+/*
+$('#acceso').submit(function(event) {
+  alert("Thank you for your comment!");
+  event.preventDefault();
+});
+*/
