510215736be20bb31d0632b6c582c8a215613ae7
Fidelz <fidel_97brea@hotmail.com>
2018-04-27 09:10:52 +0200

    artista ya muestra el nombre del artista en el titulo y los albumes (si tiene), ademas si se va a la pagina artista directamente (sin poner en la url artista=x) hace redirect a home

---
+++ js/artista_servlet.js
@@ -0,0 +1,121 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+var artista = url.searchParams.get("artista");
+if(artista == null){
+  //Si no hay artista en la url redirige al home
+  window.location = "home.html";
+}
+
+$(document).ready(function() {
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var aux = url.searchParams.get("artista");
+  var artista = $.trim(aux);
+  //Poner artista en titulo pagina
+  $("#nombre_artista_titulo").append(artista);
+
+
+  //Definir el form de mostrar albumes
+  $("#form_mostrar_albumes_artista").submit(function(event){
+      event.preventDefault(); //prevent default action
+
+      //Establecer valor del nombre de artista
+      document.getElementById("nombre_artista_album").value=artista;
+      var post_url = $(this).attr("action"); //get form action url
+      var form_data = $(this).serialize(); //Encode form elements for submission
+      var request_method = $(this).attr("method"); //get form GET/POST method
+
+      $.ajax({
+          url : post_url,
+          type: request_method,
+          data : form_data,
+
+    }).done(function(response){
+       var obj=JSON.parse(response);
+       if(obj.error != undefined){
+         if(obj.error.indexOf("Usuario no logeado en el servidor") >= 0){
+           //El usuario no esta logeado, quitar cookies e ir a inicio
+           borrarCookie("login");
+           borrarCookie("idSesion");
+           window.location = "inicio.html";
+         }
+         else{
+           //Suponer no hay albumes y mostrar sin resultados
+         }
+       }
+       else{
+         var lista_albumes = JSON.stringify(response);
+         var albumes = obj.albums;
+
+         //Añadir titulo albumes y seccion
+         if(albumes.length > 0){
+           var l='<div id="titulopagina"><h3>Álbumes</h3></div><div class="albumes"><ul id="lista_albumes"></ul></div>';
+           $("#titulopagina").after(l);
+
+           for(i=0;i<albumes.length;i++){
+             var album=albumes[i];
+             //Cambiar cuando JSON devuelva imagen
+             var image="img/blackWindows.jpg";
+             var large='<li id="barraopciones"> <div class="cancioninf"><a href="album.html'+"?album="+album+'" id="enlacecancion"><div class="imagen"><img src="'+image+'" alt="Imagen album"></div><div class="nombrecancion">'+album+'</div></a></div></li>';
+             $("#lista_albumes").append(large);
+           }
+         }
+       }
+
+    }).fail(function(response){
+        alert("Error interno. Inténtelo más tarde.");
+    });
+  });
+
+  $("#form_mostrar_albumes_artista").submit();
+
+
+  //NO ESTA HECHO ES UN GENERICO
+  $("#form_buscar_artista").submit(function(event){
+      event.preventDefault(); //prevent default action
+      //Quitar espacios en blanco a la izquierda y si no hay texto no se envia la busqueda
+      var valor_busqueda = document.getElementById("form_buscar_amigos").elements[0].value;
+      var valor_sin_espacioizquierdo = $.trim(valor_busqueda);
+      document.getElementById("search_user").value=valor_sin_espacioizquierdo;
+      if(valor_sin_espacioizquierdo == ""){
+        return false;
+      }
+
+      var post_url = $(this).attr("action"); //get form action url
+      var form_data = $(this).serialize(); //Encode form elements for submission
+      var request_method = $(this).attr("method"); //get form GET/POST method
+
+      $.ajax({
+          url : post_url,
+          type: request_method,
+          data : form_data,
+
+    }).done(function(response){
+      var obj=JSON.parse(response);
+      var lista_usuarios = JSON.stringify(response);
+      if(obj.error != undefined){
+        if(obj.error.indexOf("Usuario no logeado en el servidor") >= 0){
+          //El usuario no esta logeado, quitar cookies e ir a inicio
+          borrarCookie("login");
+          borrarCookie("idSesion");
+          window.location = "inicio.html";
+        }
+        else if(obj.error.indexOf("usuario cuyo nombre sea o empiece") >= 0){
+          sessionStorage.setItem("lista_usuarios", lista_usuarios);
+          //Pasar tambien el valor de busqueda
+          window.location= "usuarios.html?busqueda_usuario="+valor_sin_espacioizquierdo+"&pagina=1";
+        }
+        else{
+          alert("Error. Inténtelo más tarde."+obj.error);
+        }
+      }
+      else{
+        sessionStorage.setItem("lista_usuarios", lista_usuarios);
+        //Pasar tambien el valor de busqueda
+        window.location= "usuarios.html?busqueda_usuario="+valor_sin_espacioizquierdo+"&pagina=1";
+      }
+    }).fail(function(response){
+        alert("Error interno. Inténtelo más tarde.");
+    });
+  });
+});
