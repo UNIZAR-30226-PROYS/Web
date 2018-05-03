387b4d125bdf46136eee8c639b8f0b72d94057cc
Fidelz <fidel_97brea@hotmail.com>
2018-05-03 08:13:28 +0200

    añadido subir musica en esa pagina y en lista (opcion +): se ha quitado el parametro nombre (usar el de la cancion), se muestra el progreso en una barra horizontal, falta depurar con todos los mensajes posibles

--- js/lista_servlet.js
+++ js/lista_servlet.js
@@ -108,6 +108,60 @@ $(document).ready(function() {
         alert("Error interno. Inténtelo más tarde.");
     });
   });
+
+  $("#form_subir_musica").submit(function(event){
+      event.preventDefault(); //prevent default action
+      var post_url = $(this).attr("action"); //get form action url
+      var form_data = new FormData(document.getElementById("form_subir_musica")); //Encode form elements for submission
+      var request_method = $(this).attr("method"); //get form GET/POST method
+
+      $.ajax({
+          url : post_url,
+          type: request_method,
+          data : form_data,
+          dataType: "html",
+          cache: false,
+          contentType: false,
+          processData: false,
+
+          xhr: function () {
+              var xhr = new window.XMLHttpRequest();
+              xhr.upload.addEventListener("progress", function (evt) {
+                  if (evt.lengthComputable) {
+                      var percentComplete = evt.loaded / evt.total;
+                      percentComplete = parseInt(percentComplete * 100);
+                      $('.myprogress').text(percentComplete + '%');
+                      $('.myprogress').css('width', percentComplete + '%');
+                  }
+              }, false);
+              return xhr;
+          },
+
+    }).done(function(response){
+      alert(response);
+       var obj=JSON.parse(response);
+       if(obj.error != undefined){
+         if(obj.error.indexOf("Usuario no logeado en el servidor") >= 0){
+           //El usuario no esta logeado, quitar cookies e ir a inicio
+           borrarCookie("login");
+           borrarCookie("idSesion");
+           window.location = "inicio.html";
+         }
+         else{
+           $("#resultado_seguir").text(obj.error+".");
+           $("#result_seguir").attr("src","img/error.png");
+         }
+       }
+       else{
+         $("#resultado_seguir").text("Canción subida correctamente.");
+         $("#result_seguir").attr("src","img/exito.png");
+       }
+       $('.button2').click();
+
+    }).fail(function(response){
+        alert("Error interno. Inténtelo más tarde.");
+    });
+  });
 });
 
 //Actualiza la URL de la pagina cambiando el parametro key al valor value, sin hacer reload
---
+++ js/subirmusica_servlet.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  $("#form_subir_musica").submit(function(event){
+      event.preventDefault(); //prevent default action
+      var post_url = $(this).attr("action"); //get form action url
+      var form_data = new FormData(document.getElementById("form_subir_musica")); //Encode form elements for submission
+      var request_method = $(this).attr("method"); //get form GET/POST method
+
+      $.ajax({
+          url : post_url,
+          type: request_method,
+          data : form_data,
+          dataType: "html",
+          cache: false,
+          contentType: false,
+          processData: false,
+
+          xhr: function () {
+              var xhr = new window.XMLHttpRequest();
+              xhr.upload.addEventListener("progress", function (evt) {
+                  if (evt.lengthComputable) {
+                      var percentComplete = evt.loaded / evt.total;
+                      percentComplete = parseInt(percentComplete * 100);
+                      $('.myprogress').text(percentComplete + '%');
+                      $('.myprogress').css('width', percentComplete + '%');
+                  }
+              }, false);
+              return xhr;
+          },
+
+    }).done(function(response){
+      alert(response);
+       var obj=JSON.parse(response);
+       if(obj.error != undefined){
+         if(obj.error.indexOf("Usuario no logeado en el servidor") >= 0){
+           //El usuario no esta logeado, quitar cookies e ir a inicio
+           borrarCookie("login");
+           borrarCookie("idSesion");
+           window.location = "inicio.html";
+         }
+         else{
+           $("#resultado_seguir").text(obj.error+".");
+           $("#result_seguir").attr("src","img/error.png");
+         }
+       }
+       else{
+         $("#resultado_seguir").text("Canción subida correctamente.");
+         $("#result_seguir").attr("src","img/exito.png");
+       }
+       $('.button1').click();
+
+    }).fail(function(response){
+        alert("Error interno. Inténtelo más tarde.");
+    });
+  });
+
+});
