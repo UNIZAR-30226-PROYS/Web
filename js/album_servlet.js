9970514612e30d93c8f5141a09e66b92e4d50b03
Fidelz <fidel_97brea@hotmail.com>
2018-05-05 22:32:52 +0200

    pagina album dinamica, falta funcion favoritos y añadir a lista(de momento solo las muestra)

--- js/album_servlet.js
+++ js/album_servlet.js
@@ -12,9 +12,8 @@ $(document).ready(function() {
   if(c == undefined){ //Si se ha ido directamente a la pagina redirigir
     window.location="home.html";
   }
+  document.getElementById('artista_del_album').href="artista.html?artista="+c;
   $( "#titulo_artista" ).append(c);
-
-  //FALTA MOSTRAR CANCIONES
 
   $("#form_buscar_album").submit(function(event){
       event.preventDefault(); //prevent default action
@@ -63,4 +62,101 @@ $(document).ready(function() {
         alert("Error interno. Inténtelo más tarde.");
     });
   });
+
+
+  $("#form_mostrar_album").submit(function(event){
+      event.preventDefault(); //prevent default action
+      var url_string = window.location.href;
+      var url = new URL(url_string);
+      var album = url.searchParams.get("album");
+      var autor = url.searchParams.get("artista");
+
+      document.getElementById("form_mostrar_album").elements[0].value = album;
+      document.getElementById("form_mostrar_album").elements[1].value = autor;
+      var post_url = $(this).attr("action"); //get form action url
+      var form_data = $(this).serialize(); //Encode form elements for submission
+      var request_method = $(this).attr("method"); //get form GET/POST method
+
+      $.ajax({
+          url : post_url,
+          type: request_method,
+          data : form_data,
+
+    }).done(function(response){
+       var obj=JSON.parse(response);
+       if(obj.error != undefined){
+         if(obj.error.indexOf("Usuario no logeado en el servidor") >= 0){
+           //El usuario no esta logeado, quitar cookies e ir a inicio
+           borrarCookie("login");
+           borrarCookie("idSesion");
+           window.location = "inicio.html";
+         }
+       }
+       else if(obj.NoHayCanciones != undefined){
+         //No hay resultados
+          $("#artista_del_album").after("<h2 id=\"sin_resul\">No hay canciones.</h2>");
+       }
+       else{
+         var url_string = window.location.href;
+         var url = new URL(url_string);
+         var pag_actual = url.searchParams.get("pagina");
+         var autor = url.searchParams.get("artista");
+         var nombreAlbum = url.searchParams.get("album");
+
+         var canciones = obj.canciones;
+         //Definir elementos a mostrar por pagina, pagina actual y valor a empezar a mostrar
+         if(pag_actual == null){
+           pag_actual = 1;
+         }
+         else{
+           pag_actual = parseInt(pag_actual);
+         }
+         var elem_por_pagina = 4;
+         inicio=(pag_actual-1)*elem_por_pagina;
+         for(i=inicio; i<(elem_por_pagina+inicio) && i<canciones.length;i++){
+           var n_cancion=canciones[i].tituloCancion;
+           var n_artista=canciones[i].nombreArtista;
+           var n_genero=canciones[i].genero;
+           var n_album=canciones[i].nombreAlbum;
+           //Cambiar ruta por la ruta relativa
+           var ruta_aux=canciones[i].ruta;
+           var index = ruta_aux.indexOf("/ps");
+           var ruta = ".."+ruta_aux.substr(index);
+
+           var n_uploader=canciones[i].uploader;
+           //CAMBIAR IMAGEN
+           var image="img/edsheeranperfect.jpg";
+           if(n_genero==null){
+             n_genero= "";
+           }
+           if(n_album==null){
+             n_album= "";
+           }
+           var l1='<div class="cancioninf"><ul><li id="barraopciones"><a href="cancion.html?nombre='+n_cancion+'&artista='+n_artista+'&album='+n_album+'&uploader='+n_uploader+'" id="enlacecancion"><div class="imagen"><img src="'+image+'" alt="Imagen cancion"></div></a></li>';
+           var l2='<li id="barraopciones"><a href="cancion.html?nombre='+n_cancion+'&artista='+n_artista+'&album='+n_album+'&uploader='+n_uploader+'" id="enlacecancion"><div class="nombrecancion">'+n_cancion+'</div></a></li>';
+
+           var param_playmusic="\'"+ruta+"\',"+"\'"+image+"\',"+"\'"+n_cancion+"\',"+"\'"+n_artista+"\',"+"\'"+n_album+"\',"+"\'"+n_uploader+"\'";
+           var play='<li id="barraopciones"><div class="simb_repr_play"><input type="image" src="img/play.png" alt="Reproducir cancion" title="Reproducir canción" onClick="playMusic('+param_playmusic+');return false;"></div></li>';
+           //*******************MODIFICAR FAVORITOS   *****************************************
+           var fav='<form name="accionCancion" method="post" action="#"><li id="barraopciones"><div class="simb_repr_fav"><input type="image" src="img/favoritos.png" alt="Añadir a favoritos" title="Añadir/quitar favoritos" onclick="changeImage(this)"></div></li></form>';
+           var anadir_lista='<form class="form_mostrar_listas_c" method="post" action="/ps/MostrarListasReproduccion"><li id="barraopciones"><div class="simb_repr_lista"><div class="simb_repr_lista"><input type="hidden" id="user" name="user" value="'+leerCookie("login")+'"><input type="image" src="img/listas_add.png" alt="Añadir a lista" class="button" title="Añadir a lista" data-type="zoomin"></div></div></li></form>';
+           var final='</ul></div>';
+           $(".informacion").append(l1+l2+play+fav+anadir_lista+final);
+         }
+         if((elem_por_pagina+inicio)<canciones.length){
+           var pagina_sig=pag_actual+1;
+           var boton_mas = '<br><br><form action="album.html"><button type="submit" id="boton_mostrar_mas" class="aumentar">Mostrar más</button><input type="hidden" name="album" value="'+nombreAlbum+'"/><input type="hidden" name="artista" value="'+autor+'"/><input type="hidden" name="pagina" value="'+pagina_sig+'"/></form>';
+           $(".informacion").append(boton_mas);
+         }
+       }
+       //Definir form para anadir a listas
+       form_mostrar_anadir_alistas();
+
+    }).fail(function(response){
+        alert("Error interno. Inténtelo más tarde.");
+    });
+  });
+
+  $("#form_mostrar_album").submit();
+
 });
--- js/canciones_servlet.js
+++ js/canciones_servlet.js
@@ -1,52 +1,50 @@
 //MODIFICAR PARA USAR FUNCTION QUE SE LLAME CUANDO SE PONGAN LAS CANCIONES Y NO USAR TIMEOUT
 
-$(window).load(function() {
-  //Poner tiempo de espera para definir los form, sino puede que no se capture el submit
-  var explode = function(){
-    $(".form_mostrar_listas_c").submit(function(event){
-        event.preventDefault(); //prevent default action
-        $('.button').click();
-        if($(".form_anadir_cancion_lista")[0]){
-          //Si ya existe algun clase del form, es que ya se pulso, no volver a pulsar ya que sino se duplican las listas
-          return false;
-        }
-        //El user de la cookie se debe establecer al mostrar la cancion en la web
-        var post_url = $(this).attr("action"); //get form action url
-        var form_data = $(this).serialize(); //Encode form elements for submission
-        var request_method = $(this).attr("method"); //get form GET/POST method
+function form_mostrar_anadir_alistas(){
+  $(".form_mostrar_listas_c").submit(function(event){
+      event.preventDefault(); //prevent default action
+      $('.button').click();
+      if($(".form_anadir_cancion_lista")[0]){
+        //Si ya existe algun clase del form, es que ya se pulso, no volver a pulsar ya que sino se duplican las listas
+        return false;
+      }
+      //El user de la cookie se debe establecer al mostrar la cancion en la web
+      var post_url = $(this).attr("action"); //get form action url
+      var form_data = $(this).serialize(); //Encode form elements for submission
+      var request_method = $(this).attr("method"); //get form GET/POST method
 
-        $.ajax({
-            url : post_url,
-            type: request_method,
-            data : form_data,
+      $.ajax({
+          url : post_url,
+          type: request_method,
+          data : form_data,
 
-      }).done(function(response){
-         var obj=JSON.parse(response);
-         if(obj.error != undefined){
-           if(obj.error.indexOf("Usuario no logeado en el servidor") >= 0){
-             //El usuario no esta logeado, quitar cookies e ir a inicio
-             borrarCookie("login");
-             borrarCookie("idSesion");
-             window.location = "inicio.html";
-           }
-           else{
-             //No hay resultados
-             $(".close").after("<h2 id=\"sin_resul\">No hay listas.</h2>");
-           }
+    }).done(function(response){
+       var obj=JSON.parse(response);
+       if(obj.error != undefined){
+         if(obj.error.indexOf("Usuario no logeado en el servidor") >= 0){
+           //El usuario no esta logeado, quitar cookies e ir a inicio
+           borrarCookie("login");
+           borrarCookie("idSesion");
+           window.location = "inicio.html";
          }
          else{
-           var listas = obj.nombre;
-           for(i=0;i<listas.length;i++){
-             var lista=listas[i];
+           //No hay resultados
+           $(".close").after("<h2 id=\"sin_resul\">No hay listas.</h2>");
+         }
+       }
+       else{
+         var listas = obj.nombre;
+         for(i=0;i<listas.length;i++){
+           var lista=listas[i];
+           if(lista != "Favoritos"){
              var large='<form class="form_anadir_cancion_lista" action="/ps/AnyadirCancionALista" method="post"><div class="nombrelista"><input type="submit" class="anadir_cancion_lista_f" value="'+lista+'"><input type="hidden" id="nombreLista" value="'+lista+'"></div></form>';
              $(".ventana_listas").append(large);
            }
          }
+       }
 
-      }).fail(function(response){
-          alert("Error interno. Inténtelo más tarde.");
-      });
+    }).fail(function(response){
+        alert("Error interno. Inténtelo más tarde.");
     });
-  };
-  setTimeout(explode, 200);
-});
+  });
+}
